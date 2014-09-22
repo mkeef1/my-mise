75b21f4f2aa368e27644a5a26e4eeee58d574555
matt keef <mkeef1@gmail.com>
2014-09-22 14:22:29 -0500

    fixed json

--- server/test/scripts/clean-db.sh
+++ server/test/scripts/clean-db.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ] ; then
 fi
 
 mongoimport --jsonArray --drop --db $1 --collection users  --file ../../db/users.json
-mongoimport --jsonArray --drop --db $1 --collection surveys  --file ../../db/surveys.json
-mongoimport --jsonArray --drop --db $1 --collection questions --file ../../db/questions.json
-mongoimport --jsonArray --drop --db $1 --collection answers --file ../../db/answers.json
+mongoimport --jsonArray --drop --db $1 --collection recipes  --file ../../db/recipes.json
+mongoimport --jsonArray --drop --db $1 --collection favorites --file ../../db/favorites.json
+mongoimport --jsonArray --drop --db $1 --collection reviews --file ../../db/reviews.json
 
