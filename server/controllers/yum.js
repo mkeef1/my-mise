c97449403454312e5f87a086c6a3a8d2c2d57169
matt keef <mkeef1@gmail.com>
2014-10-08 12:05:56 -0500

    recipe and user saved

--- server/controllers/yum.js
+++ server/controllers/yum.js
@@ -1,15 +1,18 @@
 'use strict';
 
-var Yum = require('../models/yum');
+var Yum = require('../models/yum'),
+    User = require('../models/user');
 
 exports.index = function(req, res){
-  Yum.findAll(function(err, yums){
+  Yum.findAllByUser(req.user._id, function(err, yums){
     res.send({yums:yums});
   });
 };
 
 exports.add = function(req, res){
-  Yum.create(req.body, function(err, yum){
-    res.send({yum:yum});
+  User.findById(req.user._id, function(err, user){
+    Yum.create(req.body, req.user._id, function(err, yum){
+      res.send({yum:yum});
+    });
   });
 };
--- server/models/yum.js
+++ server/models/yum.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var Mongo = require('mongodb');
+
 function Yum(){
 }
 
@@ -7,8 +9,10 @@ Object.defineProperty(Yum, 'collection', {
   get: function(){return global.mongodb.collection('yums');}
 });
 
-Yum.create = function(yum, cb){
-  Yum.collection.save(yum, cb);
+Yum.create = function(yum, userId, cb){
+  var yumId  = Mongo.ObjectID(yumId),
+      userId = Mongo.ObjectID(userId);
+  Yum.collection.save({_id:yumId, userId:userId}, cb);
 };
 
 /*Recipe.deleteById = function(recipeId, userId, cb){
@@ -16,8 +20,8 @@ Yum.create = function(yum, cb){
   Recipe.collection.remove({_id:id, userId:userId}, cb);
 };*/
 
-Yum.findAll = function(cb){
-  Yum.collection.find().toArray(cb);
+Yum.findAllByUser = function(userId, cb){
+  Yum.collection.find({userId:userId}).toArray(cb);
 };
 
 module.exports = Yum;
