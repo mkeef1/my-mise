7234e837d95deea1eecdc4a7bc073c0feddc4a6a
matt keef <mkeef1@gmail.com>
2014-10-12 22:31:09 -0500

    recipe css

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -2,14 +2,15 @@
   'use strict';
 
   angular.module('my-mise')
-  .controller('HomeCtrl', ['$scope', 'Home', function($scope, Home){
+  .controller('HomeCtrl', ['$scope', 'Home', 'Recipeid', '$routeParams', function($scope, Home, Recipeid, $routeParams){
     $scope.title = 'My Mise';
     $scope.recipes = [];
+    $scope.recipe = {};
 
     Home.getAll().then(function(response){
       $scope.recipes = response.data.recipes;
       console.log($scope.recipes);
-      debugger;
+
     });
   }]);
 })();
